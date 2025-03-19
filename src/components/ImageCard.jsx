bef754cc62da5bb79c739592594005921eb6d9f5
LauraPastor <laurapastorj@gmail.com>
2025-03-19 12:41:20 +0100

    Add ImageCard and global styles

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,8 +1,26 @@
+import { CldImage } from "next-cloudinary";
+
 const ImageCard = ({ image }) => {
     return (
-        <div className="image-card">
-            Image CArd baby
-        </div>
+        <article className="card">
+            <div className="title-container">
+                <h4><span className="emoji">ᝰ</span>{""}</h4>
+                <h4>⫶</h4>
+            </div>
+            <CldImage />
+            <div className="controls-container">
+                <div className="control-container">
+                    <input type="checkbox" id="background" name="background" />
+                    <label htmlFor="background" >no background</label>
+                </div>
+                <div className="control-container">
+                    <input type="checkbox" id="greyscale" name="greyscale" />
+                    <label htmlFor="greyscale" >greyscale</label>
+                </div>
+                <button>↓ download</button>
+            </div>
+            <input value={""} placeholder="Start typing to change image" />
+        </article>
     );
 }
 
