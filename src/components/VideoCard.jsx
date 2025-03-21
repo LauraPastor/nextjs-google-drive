38a190e64ce2fd8c6def3c618a34f5119753477a
LauraPastor <laurapastorj@gmail.com>
2025-03-21 11:36:40 +0100

    Add VideoCard functionality

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,48 @@
 import { CldVideoPlayer } from "next-cloudinary";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { saveAs } from "file-saver";
 
-const VideoCard = ({ video }) => {
+const VideoCard = ({ asset }) => {
+    const { public_id, display_name } = asset;
     const [isLoading, setIsLoading] = useState(true);
+    const [retries, setRetries] = useState(0);
+    const [errorOccurred, setErrorOccurred] = useState(false);
+
+    const downloadOGVideo = () => {
+        const vidSrc = asset.url
+        saveAs(vidSrc, `${display_name}.mp4`);
+    }
+
+    const handleVideoError = (err) => {
+        if (err?.player?.videojs?.error_?.statusCode === 403) {
+            if (!errorOccurred) {
+                setErrorOccurred(true);
+                setIsLoading(true);
+                setRetries((prev) => prev + 1);
+                console.log("Retrying...");
+            }
+        }
+    }
+
+    const handleMetaDataLoad = () => {
+        setIsLoading(false);
+        setErrorOccurred(false);
+    }
+
+    useEffect(() => {
+        if (errorOccurred) {
+            const intervalId = setInterval(() => {
+                setRetries((prev) => prev + 1);
+            }, 5000)
+            return () => clearInterval(intervalId);
+        }
+    }, [errorOccurred])
+
+    useEffect(() => {
+        if (retries > 0 && !isLoading) {
+            setIsLoading(false);
+        }
+    }, [retries])
     return (
         <article className="card">
             <div className="title-container">
@@ -11,11 +51,20 @@ const VideoCard = ({ video }) => {
             </div>
             {isLoading && <p>Loading...</p>}
             <div className="video-container" style={{ visibility: isLoading ? "hidden" : "visible" }} >
-                {/* <CldVideoPlayer /> */}
+                <CldVideoPlayer
+                    src={public_id}
+                    id={`${public_id}-${Math.random()}`}
+                    height="300"
+                    width="300"
+                    alt={display_name}
+                    transformation={{ with: 300, height: 300, crop: "fill", gravity: "auto" }}
+                    onError={handleVideoError}
+                    onMetadataLoad={handleMetaDataLoad}
+                />
             </div>
             <div className="controls-container">
                 <div className="control-container">
-                    <button>↓ download original</button>
+                    <button onClick={downloadOGVideo} >↓ download original</button>
                 </div>
 
             </div>
